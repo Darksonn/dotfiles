14665020a0bfee61180b549fc446c6d24e1f2df9
Kristoffer Ryhl-Johansen <kristoffer@ryhl.dk>
2016-01-08 09:27:55 +0100

    Switch to jpg

--- screenshot.sh
+++ screenshot.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 basename="$(date +%s).png"
+basenamejpg="$(date +%s).jpg"
 ip='http://ryhl.moe/~user/s'
 dir='/home/user/public_html/s'
 import "$dir/$basename"
-url="$ip/$basename"
+convert "$dir/$basename" "$dir/$basenamejpg"
+url="$ip/$basenamejpg"
 echo "$url" | xclip -i -selection p
 echo "$url" | xclip -i -selection sec
 echo "$url" | xclip -i -selection clip
