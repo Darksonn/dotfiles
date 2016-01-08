1631b1ce0040246ce5c3d1075f3b8b255836aec8
Kristoffer Ryhl-Johansen <kristoffer@ryhl.dk>
2016-01-08 08:46:12 +0100

    Changed ssh connection method

--- remote_screenshot.sh
+++ remote_screenshot.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-pubkey='/home/user/.ssh/archan_id_rsa'
 basename="$(date +%s).png"
 dir='/tmp'
 import "$dir/$basename"
-scp -i "$pubkey" -P 50002 "$dir/$basename" 'user@ryhl.moe:/home/user/public_html/s'
+scp "$dir/$basename" 'archan:/home/user/public_html/s'
 ip='http://ryhl.moe/~user/s'
 url="$ip/$basename"
 echo "$url" | xclip -i -selection p
