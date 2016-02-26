fdb36a7ba363ffef73d19b8ec2b97fefd7f5233d
Kristoffer Ryhl-Johansen <kristoffer@ryhl.dk>
2016-02-26 18:18:20 +0100

    Changed /home/user to $HOME

--- rc/zstyle.sh
+++ rc/zstyle.sh
@@ -2,5 +2,5 @@
 zstyle ':completion:*' matcher-list '' 'm:{[:lower:]}={[:upper:]}' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'l:|=* r:|=*'
 zstyle ':completion:*' menu select=10
 zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
-zstyle :compinstall filename '/home/user/.zshrc'
+zstyle :compinstall filename "$HOME/.zshrc"
 
