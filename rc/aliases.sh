edd0fe8b36fc2ea55d127a42812ee044a14d138a
Alice Ryhl <alice@ryhl.io>
2017-06-06 17:50:12 +0200

    add emoji, change music hotkeys, add pdfcat alias

--- rc/aliases.sh
+++ rc/aliases.sh
@@ -5,6 +5,11 @@ function mdcd {
   mkdir -- "$1"
   cd "$1"
 }
+function pdfcat {
+  out="$1"
+  shift
+  gs -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile="$out" "$@"
+}
 # replacement for cp, also works with ssh
 alias c='rsync -avz --info=progress2'
 alias cdn='cd /home/user/Dropbox/htx/noter/'
